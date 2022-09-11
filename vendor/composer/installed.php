1e273ef56a44a98ee02cc5e0ff58611a93e9cc72
unknown <elvynmedina1993@gmail.com>
2022-09-10 23:36:17 -0400

    Salvamento funcional 1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elvyn/medina',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd32416a070c0637773a1efd26577781ccea42694',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'elvyn/medina' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd32416a070c0637773a1efd26577781ccea42694',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
