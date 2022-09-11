ff608a7393c6589ba87ea239d363378f09c2f70c
unknown <elvynmedina1993@gmail.com>
2022-09-11 00:44:46 -0400

    Se terminaron las paginas

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -13,8 +13,12 @@ class LoginController {
         echo "Desde logout";
     }
 
-    public static function olvide(){
-        echo "Desde Olvide";
+    public static function olvide(Router $router){
+        
+        $router->render('auth/olvide-password',[
+            
+        ]);
+
     }
 
     public static function recuperar(){
