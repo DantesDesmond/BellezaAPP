b841f1f407443ad48f9a88952e3006bc4e53e31e
unknown <elvynmedina1993@gmail.com>
2022-10-15 23:11:42 -0400

    Interfaz de venta completamente terminada JV agregado, resta colocar interfaz de confirmacion de servicios

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,13 @@ let paso = 1;
 const pasoInicial = 1;
 const pasoFinal = 3;
 
+const cita = {
+    nombre: '',
+    fecha: '',
+    hora: '',
+    servicios: []
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 iniciarApp();
 });
@@ -12,6 +19,7 @@ function iniciarApp() {
     botonesPaginador();// Agrega o quita los botones del paginador
     paginaSiguiente();
     paginaAnterior();
+    consultarAPI();
 
 }
 
@@ -100,4 +108,73 @@ function paginaSiguiente(){
 
         botonesPaginador();
     })
+}
+
+async function consultarAPI(){
+
+try {
+    const url = 'http://localhost:3000/api/servicios';
+    const resultado = await fetch(url);
+    const servicios = await resultado.json();
+
+    mostrarServicios(servicios);
+} catch (error) {
+    console.log(error);
+}
+
+}
+
+function mostrarServicios(servicios) {
+    servicios.forEach( servicio => {
+        const {id, nombre, precio} = servicio;
+
+        const nombreServicio = document.createElement('P');
+        nombreServicio.classList.add('nombre-servicio');
+        nombreServicio.textContent = nombre;
+
+        const precioServicio = document.createElement('P');
+        precioServicio.classList.add('precio-servicio');
+        precioServicio.textContent = `$${precio}`;
+
+        //const idServicio = document.createElement('P');
+        //idServicio.classList.add('id-servicio');
+        //idServicio.textContent = id;
+
+        const servioDiv = document.createElement('DIV');
+        servioDiv.classList.add('servicio');
+        servioDiv.dataset.idServicio = id;
+        servioDiv.onclick = function() {
+            seleccionarServicio(servicio);
+        }
+
+        servioDiv.appendChild(nombreServicio);
+        servioDiv.appendChild(precioServicio);
+
+        document.querySelector('#servicios').appendChild(servioDiv);
+    });
+}
+
+function seleccionarServicio(servicio){
+    const {id} = servicio;
+    const {servicios} = cita;
+
+
+    //Elemento click
+    const divServicio = document.querySelector(`[data-id-servicio="${id}"]`);
+
+    // Comprobar si un servicio se agrego
+    if (servicios.some( agregado => agregado.id === id ) ){
+        cita.servicios = servicios.filter(agregado => agregado.id !==id);
+        divServicio.classList.remove('seleccionado');
+
+    }else{
+        cita.servicios = [...servicios, servicio];
+        divServicio.classList.add('seleccionado');
+    }
+
+    
+
+    
+    
+    console.log(cita);
 }
